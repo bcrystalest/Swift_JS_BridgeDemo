fb140b49f403a1ea2002fcd01a29d2d4c7931633
Louis.Chen <834112686@163.com>
2018-08-02 15:44:49 +0800

    no message

---
+++ Swift_JS_BridgeDemo/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  SwiftJSBridge
+//
+//  Created by hhfa008 on 06/23/2018.
+//  Copyright (c) 2018 hhfa008. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import JavaScriptCore
+
+class ViewController: UIViewController {
+    
+    var webview:WebView?
+    var timer:Timer?
+    
+    @objc func test() -> Void {
+        
+        self.perform(#selector(test2), on: .main, with: nil, waitUntilDone: false)
+
+    }
+    
+    @objc func test2(){
+        
+        _ = self.JSBridge?.callJS(name: "sendMessageToJS", data: ["message":"Hi, I am native"]) { (data) in
+            //            print("data")
+            //            if let data = data  {
+            //                print(data)
+            //            }
+            //
+        }
+    }
+    
+    @objc func test3(){
+        _ = webview?.evaluate(javascript: "skr('skr skr')")
+    }
+    var JSBridge:SwiftJSBridge?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webview = getuiwebview()
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(test), userInfo: nil, repeats: false)
+        view.addSubview((webview?.contentView())!)
+        let JSBridge = SwiftJSBridge(for: webview!)
+        self.JSBridge = JSBridge
+        _ = JSBridge.addSwift(bridge: { (data, cb) in
+//            cb?(["appVersion":"1.0"])
+            self.perform(#selector(self.test3), on: .main, with: nil, waitUntilDone: false)
+        }, name: "getAppVersion")
+        
+        let path = Bundle.main.path(forResource: "bridge_ly", ofType: "html")
+        let file = URL.init(fileURLWithPath: path!)
+        
+        _ = webview?.loadFileURL(url: file)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func getwebview()->WKWebView {
+        let config = WKWebViewConfiguration()
+        
+        let web = WKWebView(frame: self.view.frame, configuration: config)
+        return web;
+    }
+    
+    func getuiwebview()->UIWebView {
+        let web = UIWebView(frame: self.view.frame)
+        return web;
+    }
+    
+    
+}
+
+
---
+++ Swift_JS_BridgeDemoTests/Swift_JS_BridgeDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_JS_BridgeDemoTests.swift
+//  Swift_JS_BridgeDemoTests
+//
+//  Created by 陈威利 on 2018/7/26.
+//  Copyright © 2018年 陈威利. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_JS_BridgeDemo
+
+class Swift_JS_BridgeDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
